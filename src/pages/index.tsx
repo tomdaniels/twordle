aedfc6d7e51310c74f9902d7ce27bcb5a9c4a01e
tomdaniels <thomsondaniels@hotmail.com>
2022-02-18 19:27:59 +1100

    src directory & file cleanup

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Wordle baby</title>
+        <meta name="description" content="dont be shy, 'avva go" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      hello
+    </div>
+  );
+};
+
+export default Home;
