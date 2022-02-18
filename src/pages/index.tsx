cd17f9f4d5de02a01132abcf3e118b391eea7dcd
tomdaniels <thomsondaniels@hotmail.com>
2022-02-19 09:35:59 +1100

    base key event

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
 import styles from '../styles/Home.module.css';
 
 let rows: number[] = [0, 1, 2, 3, 4, 5];
-let columns: number[] = [0, 1, 2, 3, 4, 5];
+let columns: number[] = [0, 1, 2, 3, 4];
+
 const Home: NextPage = () => {
   const [history, setHistory] = useState<string[]>([]);
   const [attempt, setAttempt] = useState<string>('');
+
+  const handleKey = (e: KeyboardEvent): void => {
+    if (attempt.length === 5) return;
+    const key = e.key.toLowerCase();
+
+    if (key === 'enter') {
+      // TODO
+      return;
+    }
+    if (key === 'backspace') {
+      // TODO
+      return;
+    }
+
+    if (/^[a-z]{1}$/.test(key)) {
+      setAttempt((attempt) => (attempt += key));
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keypress', handleKey);
+    return () => {
+      window.removeEventListener('keypress', handleKey);
+    };
+  });
 
   return (
     <div>
@@ -31,7 +57,9 @@ const Home: NextPage = () => {
                 {columns.map((colIdx) => {
                   const colId = `cell-${rowIdx}-${colIdx}`;
                   return (
-                    <div id={colId} key={colId} className={styles.cell}></div>
+                    <div id={colId} key={colId} className={styles.cell}>
+                      {rowIdx === history.length && (attempt[colIdx] || '')}
+                    </div>
                   );
                 })}
               </div>
