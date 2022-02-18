4983da3332a990c1948fb43c09360e36e3235825
tomdaniels <thomsondaniels@hotmail.com>
2022-02-19 08:18:26 +1100

    init page & header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
+import styles from "../styles/Home.module.css";
+
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Wordle baby</title>
+        <title>twordle baby</title>
         <meta name="description" content="dont be shy, 'avva go" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      hello
+      <div className={styles.container}>
+        <h1>
+          <small className={styles.small}>my clone of</small> Wordle
+        </h1>
+        <div className={styles.divider} />
+      </div>
     </div>
   );
 };
