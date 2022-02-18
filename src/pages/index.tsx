b36ae5b40a67cdb32753ee2d58a3350fe9857938
tomdaniels <thomsondaniels@hotmail.com>
2022-02-19 09:02:47 +1100

    init grid stub

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import { useState } from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
-import styles from "../styles/Home.module.css";
+import styles from '../styles/Home.module.css';
 
+let rows: number[] = [0, 1, 2, 3, 4, 5];
+let columns: number[] = [0, 1, 2, 3, 4, 5];
 const Home: NextPage = () => {
+  const [history, setHistory] = useState<string[]>([]);
+  const [attempt, setAttempt] = useState<string>('');
+
   return (
     <div>
       <Head>
@@ -16,6 +22,22 @@ const Home: NextPage = () => {
           <small className={styles.small}>my clone of</small> Wordle
         </h1>
         <div className={styles.divider} />
+
+        <div className={styles.gridWrapper}>
+          {rows.map((rowIdx) => {
+            const rowId = `row-${rowIdx}`;
+            return (
+              <div key={rowId} id={rowId} className={styles.row}>
+                {columns.map((colIdx) => {
+                  const colId = `cell-${rowIdx}-${colIdx}`;
+                  return (
+                    <div id={colId} key={colId} className={styles.cell}></div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
