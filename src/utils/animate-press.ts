c5294f0420c3bfe08871ed0d882261dae2014a05
tomdaniels <thomsondaniels@hotmail.com>
2022-02-23 12:49:19 +1100

    rmv log

--- src/utils/animate-press.ts
+++ src/utils/animate-press.ts
@@ -4,7 +4,6 @@ import animations from '../styles/Animations.module.css';
 
 function animatePress(attempt: string, history: string[]): void {
   const cell = getCell(attempt, history);
-  console.log(attempt, history);
   if (!cell) return;
   appendStyle(cell, animations.press);
   cell.style.border = '2px solid #666';
