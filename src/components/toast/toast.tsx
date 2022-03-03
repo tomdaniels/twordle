be614e32820aeedc6aaf1d2036c1cd0d8491fb7c
tomdaniels <thomsondaniels@hotmail.com>
2022-03-03 18:31:05 +1100

    toast component

---
+++ src/components/toast/toast.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+import styles from '../../styles/Toast.module.css';
+
+type ToasterProps = {
+  queue: string[];
+};
+
+const Toast: React.FC<ToasterProps> = ({ queue }) => {
+  const [toast, setToast] = useState<string[]>(queue);
+
+  useEffect(() => {
+    setToast(queue);
+  });
+
+  return (
+    <div
+      className={styles.container}
+      style={{ display: queue?.length > 0 ? 'flex' : 'none' }}
+    >
+      {toast?.length &&
+        toast.map((n, i) => {
+          return (
+            <div key={`toast-${i}`} className={styles.toast}>
+              {n}
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default Toast;
